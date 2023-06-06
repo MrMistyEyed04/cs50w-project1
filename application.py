a9ed3665c96bc0e23750520e9ca40d304b2767c5
MrMistyEyed04 <jareth.espinoza21@gmail.com>
2023-06-06 16:02:54 -0600

    animo

--- application.py
+++ application.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.orm import scoped_session, sessionmaker
 from dotenv import load_dotenv
 
+load_dotenv()
+
 app = Flask(__name__)
 
 # Check for environment variable
@@ -26,19 +28,63 @@ db = scoped_session(sessionmaker(bind=engine))
 def index():
     return render_template('index.html')
 
-@app.route("/Log_in", methods = ['GET', 'POST'])
-def Log_in ():
+@app.route("/login", methods = ['GET', 'POST'])
+def login ():
     if request.method == "POST":
-        #codigo inicio sesion
-        return "xd"
-    return render_template ('login.html')
+        if not request.form.get("username"): # Si no recibo ningún dato desde el campo username de mi html, entonces retorna una plantilla html y manda un mensaje de error
+            return render_template("login.html", message="Llena el campo username") # El message es la manipulación que yo haré para trabajar los distintos errores dentro de error.html
+        if not request.form.get("password"): # Si no recibo ningún dato desde el campo password de mi html, entonces retorda una plantilla html y manda un mensaje de error
+            return render_template("login.html", message="Llena el campo password")
 
-@app.route("/Sign_up", methods = ['GET', 'POST'])
-def Sign_up ():
+        entrada = db.execute ("SELECT * FROM users WHERE username = :username",
+                               username=request.form.get("username"))
+
+        if len(entrada) != 1 or not check_password_hash(entrada[0]["password"], request.form.get("password")):
+            return render_template("login.html", message="Nombre de usuario o contraseña incorrecto")
+
+        session["user_id"]=entrada[0]["id_user"]
+        session["username"]=entrada[0]["username"]
+        return redirect("/")
+
+    else:
+        return render_template("login.html")
+
+@app.route("/register", methods=["GET","POST"])
+def register():
     if request.method == "POST":
-        #codigo inicio sesion
-        return "xd"
-    return render_template ('signup.html')
+        username = request.form.get("username")
+        #print(username)
+        password = request.form.get("password")
+        #print(password)
+        
+        if not username or not password:
+            flash("Campos vacios")
+            return render_template('register.html')
+        
+        seleccion = text("SELECT * FROM users WHERE name = :username")
+        #print(seleccion)
+        consulta = db.execute(seleccion, {"username" : username}).fetchone()
+        print(consulta)
+        if consulta != None:
+            flash("Usuario ya existente")
+            return render_template('register.html')
+        hash = generate_password_hash(password)
+        #print(hash)from PIL import Image
+        insertar = text("INSERT INTO users (name, password) values(:username,:hash)")
+        #print(insertar)
+        db.execute(insertar, {"username" : username, "hash" : hash})
+        db.commit()
+        id = text("SELECT id FROM users WHERE name = :username")
+        variable01 = db.execute(id,{"username":username}).fetchone()[0]
+        db.commit()
+        #print("Iniciando")
+        #print(variable01)
+        #print("Finalizando")
+        session["logged_in"] = True
+        posicion = int(variable01)
+        session["users_id"] = posicion
+        return redirect("/inicio")
+    return render_template('register.html')
 
 
 
