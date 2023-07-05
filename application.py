478eaad0d88cd10b8e0d2b9353304f902b7d09c8
Guerin J. Espinoza <111297075+MrMistyEyed04@users.noreply.github.com>
2023-07-05 01:50:05 +0000

    login and register

--- application.py
+++ application.py
@@ -1,11 +1,12 @@
 import os
-
 from flask import Flask, session, render_template, redirect, request, flash,jsonify
 from flask_session import Session
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import scoped_session, sessionmaker
 from dotenv import load_dotenv
 from werkzeug.security import generate_password_hash, check_password_hash
+from helpers import login_required
+
 
 load_dotenv()
 
@@ -26,74 +27,279 @@ db = scoped_session(sessionmaker(bind=engine))
 
 
 @app.route("/")
+@login_required
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route("/login", methods = ['GET', 'POST'])
-def login ():
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """ Log user in """
+
+    # Forget any user_id
+    session.clear()
+
+    username = request.form.get("username")
+
+    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-        username = request.form.get("username")
-        password=request.form.get("password")
-        if not username: # Si no recibo ningún dato desde el campo username de mi html, entonces retorna una plantilla html y manda un mensaje de error
-            return render_template("login.html", message="Llena el campo username") # El message es la manipulación que yo haré para trabajar los distintos errores dentro de error.html
-        if not password: # Si no recibo ningún dato desde el campo password de mi html, entonces retorda una plantilla html y manda un mensaje de error
-            return render_template("login.html", message="Llena el campo password")
 
-        consulta= text("SELECT * FROM users WHERE name = :username")
-        entrada = db.execute (consulta, {"username":username}).fetchall()
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("error.html", message="must provide username")
 
-        if len(entrada) != 1 or not check_password_hash(entrada[0][2], password):
-            return render_template("login.html", message="Nombre de usuario o contraseña incorrecto")
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return render_template("error.html", message="must provide password")
 
-        session["user_id"]=entrada[0][0]
-        session["username"]=entrada[0][1]
-        return redirect("/inicio")
+        # Query database for username (http://zetcode.com/db/sqlalchemy/rawsql/)
+        # https://docs.sqlalchemy.org/en/latest/core/connections.html#sqlalchemy.engine.ResultProxy
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                            {"username": username})
+        
+        result = rows.fetchone()
 
+        # Ensure username exists and password is correct
+        if result == None or not check_password_hash(result[2], request.form.get("password")):
+            return render_template("error.html", message="invalid username and/or password")
+
+        # Remember which user has logged in
+        session["user_id"] = result[0]
+        session["user_name"] = result[1]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
-    
-@app.route("/register", methods=["GET","POST"])
-def register():
-    if request.method == "POST":
-        username = request.form.get("username")
-        #print(username)
-        password = request.form.get("password")
-        #print(password)
-        
-        if not username or not password:
-            flash("Campos vacios")
-            return render_template('register.html')
-        
-        seleccion = text("SELECT * FROM users WHERE name = :username")
-        #print(seleccion)
-        consulta = db.execute(seleccion, {"username" : username}).fetchone()
-        print(consulta)
-        if consulta != None:
-            flash("Usuario ya existente")
-            return render_template('register.html')
-        hash = generate_password_hash(password)
-        #print(hash)from PIL import Image
-        insertar = text("INSERT INTO users (name, password) values(:username,:hash)")
-        #print(insertar)
-        db.execute(insertar, {"username" : username, "hash" : hash})
-        db.commit()
-        id = text("SELECT id FROM users WHERE name = :username")
-        variable01 = db.execute(id,{"username":username}).fetchone()[0]
-        db.commit()
-        #print("Iniciando")
-        #print(variable01)
-        #print("Finalizando")
-        session["logged_in"] = True
-        posicion = int(variable01)
-        session["users_id"] = posicion
-        return redirect("/inicio")
-    return render_template('register.html')
 
-@app.route("/inicio", methods=["GET","POST"])
-def inicio():
+@app.route("/logout")
+def logout():
+    """ Log user out """
+
+    # Forget any user ID
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """ Register user """
+    
+    # Forget any user_id
+    session.clear()
+    
+    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-        datos=request.form.get("datos")
-        consulta = text("SELECT * FROM books WHERE isbn =:datos OR title =:datos OR author =:datos")
-        respuesta = db.execute(consulta,{"datos":datos}).fetchall()
-        print(respuesta)
-    return render_template("/inicio.html")
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("error.html", message="must provide username")
+
+        # Query database for username
+        userCheck = db.execute("SELECT * FROM users WHERE username = :username",
+                          {"username":request.form.get("username")}).fetchone()
+
+        # Check if username already exist
+        if userCheck:
+            return render_template("error.html", message="username already exist")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return render_template("error.html", message="must provide password")
+
+        # Ensure confirmation wass submitted 
+        elif not request.form.get("confirmation"):
+            return render_template("error.html", message="must confirm password")
+
+        # Check passwords are equal
+        elif not request.form.get("password") == request.form.get("confirmation"):
+            return render_template("error.html", message="passwords didn't match")
+        
+        # Hash user's password to store in DB
+        hashedPassword = generate_password_hash(request.form.get("password"), method='pbkdf2:sha256', salt_length=8)
+        
+        # Insert register into DB
+        db.execute("INSERT INTO users (username, hash) VALUES (:username, :password)",
+                            {"username":request.form.get("username"), 
+                             "password":hashedPassword})
+
+        # Commit changes to database
+        db.commit()
+
+        flash('Account created', 'info')
+
+        # Redirect user to login page
+        return redirect("/login")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+
+@app.route("/search", methods=["GET"])
+@login_required
+def search():
+    """ Get books results """
+
+    # Check book id was provided
+    if not request.args.get("book"):
+        return render_template("error.html", message="you must provide a book.")
+
+    # Take input and add a wildcard
+    query = "%" + request.args.get("book") + "%"
+
+    # Capitalize all words of input for search
+    # https://docs.python.org/3.7/library/stdtypes.html?highlight=title#str.title
+    query = query.title()
+    
+    rows = db.execute("SELECT isbn, title, author, year FROM books WHERE \
+                        isbn LIKE :query OR \
+                        title LIKE :query OR \
+                        author LIKE :query LIMIT 15",
+                        {"query": query})
+    
+    # Books not founded
+    if rows.rowcount == 0:
+        return render_template("error.html", message="we can't find books with that description.")
+    
+    # Fetch all the results
+    books = rows.fetchall()
+
+    return render_template("results.html", books=books)
+
+@app.route("/book/<isbn>", methods=['GET','POST'])
+@login_required
+def book(isbn):
+    """ Save user review and load same page with reviews updated."""
+
+    if request.method == "POST":
+
+        # Save current user info
+        currentUser = session["user_id"]
+        
+        # Fetch form data
+        rating = request.form.get("rating")
+        comment = request.form.get("comment")
+        
+        # Search book_id by ISBN
+        row = db.execute("SELECT id FROM books WHERE isbn = :isbn",
+                        {"isbn": isbn})
+
+        # Save id into variable
+        bookId = row.fetchone() # (id,)
+        bookId = bookId[0]
+
+        # Check for user submission (ONLY 1 review/user allowed per book)
+        row2 = db.execute("SELECT * FROM reviews WHERE user_id = :user_id AND book_id = :book_id",
+                    {"user_id": currentUser,
+                     "book_id": bookId})
+
+        # A review already exists
+        if row2.rowcount == 1:
+            
+            flash('You already submitted a review for this book', 'warning')
+            return redirect("/book/" + isbn)
+
+        # Convert to save into DB
+        rating = int(rating)
+
+        db.execute("INSERT INTO reviews (user_id, book_id, comment, rating) VALUES \
+                    (:user_id, :book_id, :comment, :rating)",
+                    {"user_id": currentUser, 
+                    "book_id": bookId, 
+                    "comment": comment, 
+                    "rating": rating})
+
+        # Commit transactions to DB and close the connection
+        db.commit()
+
+        flash('Review submitted!', 'info')
+
+        return redirect("/book/" + isbn)
+    
+    # Take the book ISBN and redirect to his page (GET)
+    else:
+
+        row = db.execute("SELECT isbn, title, author, year FROM books WHERE \
+                        isbn = :isbn",
+                        {"isbn": isbn})
+
+        bookInfo = row.fetchall()
+
+        """ GOODREADS reviews """
+
+        # Read API key from env variable
+        key = os.getenv("GOODREADS_KEY")
+        
+        # Query the api with key and ISBN as parameters
+        query = requests.get("https://www.goodreads.com/book/review_counts.json",
+                params={"key": key, "isbns": isbn})
+
+        # Convert the response to JSON
+        response = query.json()
+
+        # "Clean" the JSON before passing it to the bookInfo list
+        response = response['books'][0]
+
+        # Append it as the second element on the list. [1]
+        bookInfo.append(response)
+
+        """ Users reviews """
+
+         # Search book_id by ISBN
+        row = db.execute("SELECT id FROM books WHERE isbn = :isbn",
+                        {"isbn": isbn})
+
+        # Save id into variable
+        book = row.fetchone() # (id,)
+        book = book[0]
+
+        # Fetch book reviews
+        # Date formatting (https://www.postgresql.org/docs/9.1/functions-formatting.html)
+        results = db.execute("SELECT users.username, comment, rating, \
+                            to_char(time, 'DD Mon YY - HH24:MI:SS') as time \
+                            FROM users \
+                            INNER JOIN reviews \
+                            ON users.id = reviews.user_id \
+                            WHERE book_id = :book \
+                            ORDER BY time",
+                            {"book": book})
+
+        reviews = results.fetchall()
+
+        return render_template("book.html", bookInfo=bookInfo, reviews=reviews)
+
+@app.route("/api/<isbn>", methods=['GET'])
+@login_required
+def api_call(isbn):
+
+    # COUNT returns rowcount
+    # SUM returns sum selected cells' values
+    # INNER JOIN associates books with reviews tables
+
+    row = db.execute("SELECT title, author, year, isbn, \
+                    COUNT(reviews.id) as review_count, \
+                    AVG(reviews.rating) as average_score \
+                    FROM books \
+                    INNER JOIN reviews \
+                    ON books.id = reviews.book_id \
+                    WHERE isbn = :isbn \
+                    GROUP BY title, author, year, isbn",
+                    {"isbn": isbn})
+
+    # Error checking
+    if row.rowcount != 1:
+        return jsonify({"Error": "Invalid book ISBN"}), 422
+
+    # Fetch result from RowProxy    
+    tmp = row.fetchone()
+
+    # Convert to dict
+    result = dict(tmp.items())
+
+    # Round Avg Score to 2 decimal. This returns a string which does not meet the requirement.
+    # https://floating-point-gui.de/languages/python/
+    result['average_score'] = float('%.2f'%(result['average_score']))
+
+    return jsonify(result)  
